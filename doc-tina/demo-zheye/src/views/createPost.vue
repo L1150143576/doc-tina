d30b1ea81ab987aa95e65ef9fe79d0a4d661ac39
L1150143576 <1150143576@qq.com>
2020-11-22 15:33:41 +0800

    feat:创建文章

---
+++ doc-tina/demo-zheye/src/views/createPost.vue
@@ -0,0 +1,73 @@
+<template>
+	<div>
+		<h2>新建文章</h2>
+		<validate-form @form-submit="onFormSubmit">
+			<div class="mb-3">
+				<label class="form-label">文章标题：</label>
+				<validate-input
+					:rules="titleRules"
+					v-model="titleVal"
+					placeholder="请输入文章标题"
+					type="text"
+				/>
+			</div>
+			<div class="mb-3">
+				<label class="form-label">文章详情：</label>
+				<validate-input
+					rows="10"
+					tag="textarea"
+					placeholder="请输入文章详情"
+					:rules="contentRules"
+					v-model="contentVal"
+				/>
+			</div>
+			<template #submit>
+				<button class="btn btn-primary btn-large">发表文章</button>
+			</template>
+		</validate-form>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { GlobalDataProps, PostProps } from '../store'
+import ValidateForm from '@/components/ValidateForm.vue'
+import ValidateInput, { RulesProp } from '../components/ValidateInput.vue'
+import { useStore } from 'vuex'
+export default defineComponent({
+	setup() {
+		const store = useStore<GlobalDataProps>()
+		const router = useRouter()
+		const contentVal = ref('')
+		const titleVal = ref('')
+		const contentRules: RulesProp = [
+			{ type: 'required', message: '文章详情不能为空' },
+		]
+		const titleRules: RulesProp = [
+			{ type: 'required', message: '文章标题不能为空' },
+		]
+		const onFormSubmit = (result: boolean) => {
+			if (result) {
+				const { columnId } = store.state.user
+				if (columnId) {
+					const newPost: PostProps = {
+						_id: new Date().getTime().toString(),
+						title: titleVal.value,
+						content: contentVal.value,
+						columnId,
+						createdAt: new Date().toLocaleString(),
+					}
+					store.commit('createPost', newPost)
+					router.push({ name: 'Detail', params: { id: columnId } })
+				}
+			}
+		}
+		return { contentVal, titleVal, contentRules, onFormSubmit, titleRules }
+	},
+	components: {
+		ValidateForm,
+		ValidateInput,
+	},
+})
+</script>
