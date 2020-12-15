17d3c46669e6b646672c3540a7f7bdccccefb75d
L1150143576 <1150143576@qq.com>
2020-12-15 23:19:13 +0800

    feat:test

---
+++ doc-tina/vue3-component/test/unit/example.spec.ts
@@ -0,0 +1,26 @@
+import { shallowMount } from "@vue/test-utils";
+import "jest";
+import { defineComponent, h } from "vue";
+const HelloWorld = defineComponent({
+  name: "helloworld",
+  props: {
+    msg: {
+      type: String,
+    },
+  },
+  setup(props) {
+    return () => {
+      return h("div", props.msg);
+    };
+  },
+});
+
+describe("HelloWorld.vue", () => {
+  test("is a Vue instance", () => {
+    const msg = "new message";
+    const wrapper = shallowMount(HelloWorld as any, {
+      props: { msg },
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
