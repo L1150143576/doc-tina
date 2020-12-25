ce0f1110f5f7b80e89686e3ad0aea96252976df4
L1150143576 <l>
2020-12-25 08:31:39 +0800

    feat:源码示例

--- doc-tina/vue3-source/src/App.tsx
+++ doc-tina/vue3-source/src/App.tsx
@@ -1,11 +1,18 @@
-import { defineComponent, reactive, ref } from 'vue'
+import { defineComponent, reactive, ref, shallowReactive } from 'vue'
+
 const A = defineComponent({
   setup() {
-    const state = reactive({
-      name: ref('Tina')
-    })
+    const state = shallowReactive(new Map())
+    const myRef = ref({})
+
+    console.log(myRef)
+    state.set('count', 1)
+    // setInterval(() => {
+    //   console.log('开始计数辣~')
+    //   state.set('count', state.get('count') + 1)
+    // }, 1000)
     return () => {
-      return <div>{state.name}</div>
+      return <div>{state.get('count')}</div>
     }
   }
 })
