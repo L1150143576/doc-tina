cb039fbd8818ccd2a4933fac659bba22790aeec1
L1150143576 <l>
2021-01-13 08:27:31 +0800

    feta:规划

--- doc-tina/vue3-source/src/App.tsx
+++ doc-tina/vue3-source/src/App.tsx
@@ -1,18 +1,31 @@
-import { defineComponent, reactive, ref, shallowReactive } from 'vue'
+import { defineComponent, reactive, ref, shallowReactive, watch, watchEffect } from 'vue'
 
 const A = defineComponent({
   setup() {
-    const state = shallowReactive(new Map())
-    const myRef = ref({})
+    // const state = shallowReactive(new Map())
+    // const myRef = ref({})
 
-    console.log(myRef)
-    state.set('count', 1)
+    // console.log(myRef)
+    // state.set('count', 1)
+    const state = reactive({
+      count: 0
+    })
     // setInterval(() => {
     //   console.log('开始计数辣~')
     //   state.set('count', state.get('count') + 1)
     // }, 1000)
+    watchEffect((onInvalidate) => {
+      console.log(state.count)
+      onInvalidate(() => {})
+    })
+    watch(
+      () => state.count,
+      (nv, ov) => {
+        console.log(nv, ov)
+      }
+    )
     return () => {
-      return <div>{state.get('count')}</div>
+      return <div>{state.count}</div>
     }
   }
 })
