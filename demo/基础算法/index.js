7b2029449dc5c1f48bdee28cb8cd40dd0cb23079
L1150143576 <1150143576@qq.com>
2021-03-02 16:03:06 +0800

    基础算法

--- demo/基础算法/index.js
+++ demo/基础算法/index.js
@@ -1,19 +1,18 @@
 //bubble排序
 Array.prototype.bubblesSort = function () {
   //length:6
-  for (let i  = 0; i < this.length - 1; i++){ //1
-    //  减i有效减少轮次
-      for (let j = 0; j < this.length - 1 - i; j++){ //3
-          if(this[j] > this[j + 1]){//2,0
-              let temp = this[j];
-              this[j] = this[j + 1];
-              this[j + 1] = temp;
-          }
-      }
+  for(let i=0;i<this.length;i++){
+     for(let j=0;j<this.length-1-i;j++){
+       if(this[j]>this[j+1]){
+         let temp=this[j+1]
+         this[j+1]=this[j]
+         this[j]=temp
+       }
+     }
   }
   return this
 };
-// const arr = [5,4,3,2,1,0];
+// const arr = [5,4,3,2,1,0,8];
 // console.log(arr.bubblesSort())
 
 /**
@@ -21,19 +20,22 @@ Array.prototype.bubblesSort = function () {
  * 最小值排第一位、第二小值排第二位。以此类推
  */
 Array.prototype.selectionSort = function () {
-   for(let i=0;i<this.length-1;i++){
+   for(let i=0;i<this.length;i++){
+     let indexMid=i
      for(let j=i;j<this.length;j++){
-       if(this[j]<this[i]){
-         const temp=this[i]
-         this[i]=this[j]
-         this[j]=temp
-       }
+         if(this[j]<this[indexMid]){
+           console.log(j,this[j])
+           indexMid=j
+         }
      }
+     const temp=this[i]
+     this[i]=this[indexMid]
+     this[indexMid]=temp
    }
    return this
 };
 // const arr = [5,2, 4, 3, 2, 1];
-// arr.selectionSort();
+// console.log(arr.selectionSort());
 
 /**
  * 插入排序
@@ -42,42 +44,40 @@ Array.prototype.selectionSort = function () {
 
 Array.prototype.insertionSort = function () {
  var len=this.length
- var current,preIndex;
- for(let i=0;i<len;i++){
-   preIndex=i-1
-   current=arr[i]
-   while(preIndex>=0&&this[preIndex]>current){
-        this[preIndex+1]=this[preIndex]
-        preIndex--
-   }
-   this[preIndex+1]=current
- }
+ var preIndex, current;
+  for(let i=0;i<len;i++){
+    preIndex=i-1
+    current=this[i]
+    while(preIndex>=0&&current<this[preIndex]){
+     this[i]=this[preIndex]
+     this[preIndex]=current
+      preIndex--
+    }
+  }
   return this;
 };
-// const arr = [5,4,3,2,1,0];
-// console.log(arr.insertionSort())
+const arr = [5,4,3,2,1,0];
+console.log(arr.insertionSort())
 
 /**
  * 快速排序
  *  分区然后排序、性能较好、可以项目中使用
  */
 Array.prototype.quickSort = function () {
-  const rec = (arr) => {
-    if(arr.length==1) return arr
-    let left=[],
-        right=[],
-        mid=arr[0]
-    for(let i=1;i<arr.length;i++){
-      if(arr[i]<mid){
-        left.push(arr[i])
-      }else{
-        right.push(arr[i])
-      }
-    }
-    return [...rec(left),mid,...rec(right)]
-
-  };
-  return rec(this);
+   const rec=function(arr){
+        // 递归做好跳出条件
+   if (arr.length === 1) {return arr}
+     let mid=arr[0],left=[],right=[]
+     for(let i=1;i<arr.length;i++){
+       if(arr[i]<mid){
+         left.push(arr[i])
+       }else{
+         right.push(arr[i])
+       }
+     }
+     return [...rec(left),mid,...rec(right)]
+   }
+   return rec(this)
 };
-const arr = [2,4,5,3,1];
-console.log(arr.quickSort());
+// const arr = [2,4,5,3,1];
+// console.log(arr.quickSort());
