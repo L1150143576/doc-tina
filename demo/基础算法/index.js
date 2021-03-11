3a9002f3e22adc950ac7c3600b4e0752d90f8d73
L1150143576 <1150143576@qq.com>
2021-03-11 17:12:19 +0800

    commit

--- demo/基础算法/index.js
+++ demo/基础算法/index.js
@@ -1,16 +1,16 @@
 //bubble排序
 Array.prototype.bubblesSort = function () {
   //length:6
-  for(let i=0;i<this.length;i++){
-     for(let j=0;j<this.length-1-i;j++){
-       if(this[j]>this[j+1]){
-         let temp=this[j+1]
-         this[j+1]=this[j]
-         this[j]=temp
-       }
-     }
+  for (let i = 0; i < this.length; i++) {
+    for (let j = 0; j < this.length - 1 - i; j++) {
+      if (this[j] > this[j + 1]) {
+        let temp = this[j + 1];
+        this[j + 1] = this[j];
+        this[j] = temp;
+      }
+    }
   }
-  return this
+  return this;
 };
 // const arr = [5,4,3,2,1,0,8];
 // console.log(arr.bubblesSort())
@@ -20,19 +20,19 @@ Array.prototype.bubblesSort = function () {
  * 最小值排第一位、第二小值排第二位。以此类推
  */
 Array.prototype.selectionSort = function () {
-   for(let i=0;i<this.length;i++){
-     let indexMid=i
-     for(let j=i;j<this.length;j++){
-         if(this[j]<this[indexMid]){
-           console.log(j,this[j])
-           indexMid=j
-         }
-     }
-     const temp=this[i]
-     this[i]=this[indexMid]
-     this[indexMid]=temp
-   }
-   return this
+  for (let i = 0; i < this.length; i++) {
+    let indexMid = i;
+    for (let j = i; j < this.length; j++) {
+      if (this[j] < this[indexMid]) {
+        console.log(j, this[j]);
+        indexMid = j;
+      }
+    }
+    const temp = this[i];
+    this[i] = this[indexMid];
+    this[indexMid] = temp;
+  }
+  return this;
 };
 // const arr = [5,2, 4, 3, 2, 1];
 // console.log(arr.selectionSort());
@@ -43,41 +43,45 @@ Array.prototype.selectionSort = function () {
  */
 
 Array.prototype.insertionSort = function () {
- var len=this.length
- var preIndex, current;
-  for(let i=0;i<len;i++){
-    preIndex=i-1
-    current=this[i]
-    while(preIndex>=0&&current<this[preIndex]){
-     this[i]=this[preIndex]
-     this[preIndex]=current
-      preIndex--
+  var len = this.length;
+  var preIndex, current;
+  for (let i = 1; i < len; i++) {
+    preIndex = i - 1;
+    current = this[i];
+    while (preIndex >= 0 && this[preIndex] > current) {
+      this[preIndex + 1] = this[preIndex];
+      preIndex--;
     }
+    this[preIndex + 1] = current;
   }
   return this;
 };
-const arr = [5,4,3,2,1,0];
-console.log(arr.insertionSort())
+const arr = [5, 4, 3, 2, 1, 0];
+console.log(arr.insertionSort());
 
 /**
  * 快速排序
  *  分区然后排序、性能较好、可以项目中使用
  */
 Array.prototype.quickSort = function () {
-   const rec=function(arr){
-        // 递归做好跳出条件
-   if (arr.length === 1) {return arr}
-     let mid=arr[0],left=[],right=[]
-     for(let i=1;i<arr.length;i++){
-       if(arr[i]<mid){
-         left.push(arr[i])
-       }else{
-         right.push(arr[i])
-       }
-     }
-     return [...rec(left),mid,...rec(right)]
-   }
-   return rec(this)
+  const rec = function (arr) {
+    // 递归做好跳出条件
+    if (arr.length === 1) {
+      return arr;
+    }
+    let mid = arr[0],
+      left = [],
+      right = [];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < mid) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+    return [...rec(left), mid, ...rec(right)];
+  };
+  return rec(this);
 };
 // const arr = [2,4,5,3,1];
 // console.log(arr.quickSort());
--- demo/简单例子/index.js
+++ demo/简单例子/index.js
@@ -1,35 +1,46 @@
-Function.prototype.myCall=function(context){
-  context.fn=this
-  const otherArgus=[...arguments].slice(1)
-  const result=context.fn(otherArgus)
-  delete context.fn
-  return result
-}
+Function.prototype.myCall = function (context) {
+  context.fn = this;
+  const otherArgus = [...arguments].slice(1);
+  const result = context.fn(otherArgus);
+  delete context.fn;
+  return result;
+};
 
-Function.prototype.myApply=function(context,arr){
-  context.fn=this
+Function.prototype.myApply = function (context, arr) {
+  context.fn = this;
   let result;
-  if(arr){
-    result=context.fn(arr)
-  }else{
-    result=context.fn()
+  if (arr) {
+    result = context.fn(arr);
+  } else {
+    result = context.fn();
   }
-  delete context.fn
-  return result
-}
+  delete context.fn;
+  return result;
+};
 
-Function.prototype.myBind=function(context){
-  const fn=this
-  const otherArgus=[...arguments].slice(1)
-  const result=function(){
-    const resultArgus=[...arguments]
-    if(this instanceof result){
-      fn.apply(this,otherArgus.concat(resultArgus))
-    }else{
-      fn.apply(context,otherArgus.concat(resultArgus))
+Function.prototype.myBind = function (context) {
+  const fn = this;
+  const otherArgus = [...arguments].slice(1);
+  const result = function () {
+    const resultArgus = [...arguments];
+    if (this instanceof result) {
+      fn.apply(this, otherArgus.concat(resultArgus));
+    } else {
+      fn.apply(context, otherArgus.concat(resultArgus));
     }
-  }
+  };
 
-  result.prototype=Object.create(fn.prototype)
-  return result
+  result.prototype = Object.create(fn.prototype);
+  return result;
+};
+
+function sum() {
+  var sum = 0,
+    i = 1;
+  while (i <= 100) {
+    if (i % 3 == 0) sum += i;
+    i++;
+  }
+  return sum;
 }
+console.log(sum());
