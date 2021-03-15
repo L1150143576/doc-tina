3267e45f96ac16151dfb80551dbd4241e1988e2a
L1150143576 <1150143576@qq.com>
2021-03-15 14:39:28 +0800

    feat:防抖

--- demo/简单例子/index.js
+++ demo/简单例子/index.js
@@ -23,6 +23,7 @@ Function.prototype.myBind = function (context) {
   const otherArgus = [...arguments].slice(1);
   const result = function () {
     const resultArgus = [...arguments];
+    //是否通过new对象
     if (this instanceof result) {
       fn.apply(this, otherArgus.concat(resultArgus));
     } else {
@@ -43,4 +44,71 @@ function sum() {
   }
   return sum;
 }
-console.log(sum());
+// console.log(sum())
+var value = 1;
+var foo = {
+  value: 2,
+  bar: function () {
+    return this.value;
+  },
+};
+// var fn = foo.bar;
+// console.log(fn);
+// console.log(fn());
+var value = 1;
+var foo = {
+  value: 1,
+};
+
+function bar() {
+  console.log(this.value);
+}
+// bar.call(foo); //
+
+function myNew(func, ...args) {
+  // 1. 判断方法体
+  if (typeof func !== "function") {
+    throw "第一个参数必须是方法体";
+  }
+
+  // 2. 创建新对象
+  const obj = {};
+
+  // 3. 这个对象的 __proto__ 指向 func 这个类的原型对象
+  // 即实例可以访问构造函数原型（constructor.prototype）所在原型链上的属性
+  obj.__proto__ = Object.create(func.prototype);
+
+  // 为了兼容 IE 可以让步骤 2 和 步骤 3 合并
+  // const obj = Object.create(func.prototype);
+
+  // 4. 通过 apply 绑定 this 执行并且获取运行后的结果
+  let result = func.apply(obj, args);
+
+  // 5. 如果构造函数返回的结果是引用数据类型，则返回运行后的结果
+  // 否则返回新创建的 obj
+  const isObject = typeof result === "object" && result !== null;
+  const isFunction = typeof result === "function";
+  return isObject || isFunction ? result : obj;
+}
+
+// 测试
+function Person() {}
+Person.prototype.name = "Tina";
+Person.prototype.age = "18";
+// 用来测试第 2 点和第 3 点
+Person.prototype.sayName = function () {
+  console.log(`My name is ${this.name}`);
+};
+var proto = Object.create(Person.prototype);
+function Superman() {}
+Superman.prototype = proto;
+Superman.prototype.power = "power";
+Superman.prototype.sayPower = function () {
+  console.log(`My power is ${this.power}`);
+};
+var superman = new Superman("你好");
+console.log(superman.power);
+console.log(superman.name);
+console.log(superman.age);
+console.log(superman.sayPower());
+console.log(superman.sayName());
---
+++ demo/简单例子/new和继承.js
@@ -0,0 +1,41 @@
+function myNew(func, ...args) {
+  // 1. 判断方法体
+  if (typeof func !== "function") {
+    throw "第一个参数必须是方法体";
+  }
+
+  // 2. 创建新对象
+  const obj = {};
+
+  // 3. 这个对象的 __proto__ 指向 func 这个类的原型对象
+  // 即实例可以访问构造函数原型（constructor.prototype）所在原型链上的属性
+  obj.__proto__ = Object.create(func.prototype);
+
+  // 为了兼容 IE 可以让步骤 2 和 步骤 3 合并
+  // const obj = Object.create(func.prototype);
+
+  // 4. 通过 apply 绑定 this 执行并且获取运行后的结果
+  let result = func.apply(obj, args);
+
+  // 5. 如果构造函数返回的结果是引用数据类型，则返回运行后的结果
+  // 否则返回新创建的 obj
+  const isObject = typeof result === "object" && result !== null;
+  const isFunction = typeof result === "function";
+  return isObject || isFunction ? result : obj;
+}
+
+// 测试
+function Person(name) {
+  this.name = name;
+  return function () {
+    // 用来测试第 5 点
+    console.log("返回引用数据类型");
+  };
+}
+// 用来测试第 2 点和第 3 点
+Person.prototype.sayName = function () {
+  console.log(`My name is ${this.name}`);
+};
+const me = myNew(Person, "jsliang"); // 用来测试第 4 点
+me.sayName(); // My name is jsliang
+console.log(me); // Person {name: 'jsliang'}
