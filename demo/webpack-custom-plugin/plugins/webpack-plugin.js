e71cb28592db0e64146c0ec9ad1bd86937882fcd
L1150143576 <1150143576@qq.com>
2021-08-22 17:22:59 +0800

    webpack-basic-demo

---
+++ demo/webpack-custom-plugin/plugins/webpack-plugin.js
@@ -0,0 +1,12 @@
+// 1. 创建一个构造函数
+function No1WebpackPlugin(options) {
+  this.options = options;
+}
+// 2. 重写构造函数原型对象上的 apply 方法
+No1WebpackPlugin.prototype.apply = function (compiler) {
+  compiler.hooks.done.tap("done", () => {
+    console.log(this.options.msg);
+  });
+};
+// 3. 将我们的自定义插件导出
+module.exports = No1WebpackPlugin;
